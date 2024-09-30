b8943809afcb8cdf421c7506db33ef531f36e699
Marie-Sail <marie.saillard.pro@gmail.com>
2024-09-30 18:36:24 +0200

    Challenge 6 - contact form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { ArticlePageComponent } from './Pages/article-page/article-page.component';
+import { NotFoundComponent } from './Pages/not-found/not-found.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: HomePageComponent},
+    {path:'article/:id', component: ArticlePageComponent},
+   {path:'form',component:ContactFormComponent}, 
+   {path:'**', component:NotFoundComponent},
+    
+];
