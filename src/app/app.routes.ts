4429647e7b7edd62b353ceab424bfa1dedfb6596
Marie-Sail <marie.saillard.pro@gmail.com>
2024-10-01 17:06:04 +0200

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SignupFormComponent } from './Pages/signup-form/signup-form.component';
 
 export const routes: Routes = [
-    {path:'', component: HomePageComponent},
+    {path:'', component: HomePageComponent, pathMatch: 'full'},
     {path:'article/:id', component: ArticlePageComponent},
    {path:'form',component:SignupFormComponent}, 
    {path:'**', component:NotFoundComponent},
