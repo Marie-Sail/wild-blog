45e5d57b189f07e8551db9c4022b6c320f81ebe9
Marie-Sail <marie.saillard.pro@gmail.com>
2024-11-14 15:27:01 +0100

    challenge 11 Services

---
+++ src/app/Services/api.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Article } from '../Models/article.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor() { }
+
+  private apiUrl = 'http://localhost:3000/articles';
+
+  private http = inject(HttpClient);
+
+  getArticles(): Observable<Article[]> {
+    return this.http.get<Article[]>(this.apiUrl);
+  }
+
+  // Méthode pour récupérer un article par id
+  getArticleById(id: number): Observable<Article> {
+    return this.http.get<Article>(`${this.apiUrl}/${id}`);
+  }
+
+
+}
