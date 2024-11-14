60f40e9054920e47e1f02ed48193fc38dd3d0e44
Marie-Sail <marie.saillard.pro@gmail.com>
2024-11-14 16:27:27 +0100

    asyncPipe

--- src/app/Pages/article-page/article-page.component.ts
+++ src/app/Pages/article-page/article-page.component.ts
@@ -3,12 +3,14 @@ import { ArticleComponent } from "../../ArticleComponent/article.component";
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Article } from '../../Models/article.model';
 import { ApiService } from '../../Services/api.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-article-page',
   standalone: true,
-  imports: [ArticleComponent],
+  imports: [ArticleComponent, CommonModule],
   templateUrl: './article-page.component.html',
   styleUrl: './article-page.component.scss'
 })
@@ -19,22 +21,19 @@ export class ArticlePageComponent {
   
   articleId!: number;
 
-  article!: Article;
-
-  // article$!:Observable<Article>
+  article$!:Observable<Article>;
 
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.articleId = Number(params.get('id'));
+      this.article$ = this.apiService.getArticleById(this.articleId)
+    
     });
     
-    this.apiService.getArticleById(this.articleId).subscribe(data => {
-              this.article = data;
-            },
-            );
+   
 
   }
 
-  
+
 }
